dcd855a656b3b36d3bf7c20a33591125a144f073
Marco Arment <me@marco.org>
2015-01-16 21:35:16 -0500

    Added access groups to keychain

--- FCUtilities/FCSimpleKeychain.h
+++ FCUtilities/FCSimpleKeychain.h
@@ -6,17 +6,20 @@
 #ifndef FCSimpleKeychain_h
 #define FCSimpleKeychain_h
 
+
 @import Security;
 
-static __inline__ __attribute__((always_inline)) NSString *fc_keychainStringForKey(NSString *key, BOOL *outKeychainError)
+static __inline__ __attribute__((always_inline)) NSString *fc_groupKeychainStringForKey(NSString *key, BOOL *outKeychainError, NSString *accessGroup)
 {
     CFDataRef data = nil;
-    OSStatus err = SecItemCopyMatching((__bridge CFDictionaryRef) @{
+    NSMutableDictionary *params = [@{
         (__bridge id) kSecClass : (__bridge id) kSecClassGenericPassword,
         (__bridge id) kSecAttrService : [[NSBundle.mainBundle.infoDictionary objectForKey:(NSString *)kCFBundleIdentifierKey] stringByAppendingFormat:@".%@", key],
-        (__bridge id) kSecReturnData : (__bridge id) kCFBooleanTrue
-    }, (CFTypeRef *) &data);
-    
+        (__bridge id) kSecReturnData : (__bridge id) kCFBooleanTrue,
+    } mutableCopy];
+    if (accessGroup) params[(__bridge id) kSecAttrAccessGroup] = accessGroup;
+
+    OSStatus err = SecItemCopyMatching((__bridge CFDictionaryRef) params, (CFTypeRef *) &data);
     if (err != errSecSuccess && err != errSecItemNotFound) {
         NSLog(@"Keychain error: SecItemCopyMatching failed for key %@: %d", key, (int) err);
         if (outKeychainError) *outKeychainError = YES;
@@ -28,31 +31,57 @@ static __inline__ __attribute__((always_inline)) NSString *fc_keychainStringForK
     return [[NSString alloc] initWithData:(__bridge_transfer NSData *)data encoding:NSUTF8StringEncoding];
 }
 
-static __inline__ __attribute__((always_inline)) BOOL fc_deleteKeychainStringForKey(NSString *key)
+static __inline__ __attribute__((always_inline)) BOOL fc_deleteGroupKeychainStringForKey(NSString *key, NSString *accessGroup)
 {
-    return errSecSuccess == SecItemDelete((__bridge CFDictionaryRef) @{
+    NSMutableDictionary *params = [@{
         (__bridge id) kSecClass : (__bridge id) kSecClassGenericPassword,
         (__bridge id) kSecAttrService : [[NSBundle.mainBundle.infoDictionary objectForKey:(NSString *)kCFBundleIdentifierKey] stringByAppendingFormat:@".%@", key],
-    });
+    } mutableCopy];
+    if (accessGroup) params[(__bridge id) kSecAttrAccessGroup] = accessGroup;
+    return errSecSuccess == SecItemDelete((__bridge CFDictionaryRef) params);
 }
 
-static __inline__ __attribute__((always_inline)) BOOL fc_setKeychainStringForKeyWithAccessibility(NSString *key, NSString *value, CFTypeRef accessibility)
+static __inline__ __attribute__((always_inline)) BOOL fc_setGroupKeychainStringForKeyWithAccessibility(NSString *key, NSString *value, CFTypeRef accessibility, NSString *accessGroup)
 {
-    if (! value) return fc_deleteKeychainStringForKey(key);
+    if (! value) return fc_deleteGroupKeychainStringForKey(key, accessGroup);
 
-    NSDictionary *query = @{
+    NSMutableDictionary *query = [@{
         (__bridge id) kSecClass : (__bridge id) kSecClassGenericPassword,
         (__bridge id) kSecAttrService : [[NSBundle.mainBundle.infoDictionary objectForKey:(NSString *)kCFBundleIdentifierKey] stringByAppendingFormat:@".%@", key],
         (__bridge id) kSecAttrAccessible : (__bridge id) accessibility,
         (__bridge id) kSecValueData : [value dataUsingEncoding:NSUTF8StringEncoding]
-    };
+    } mutableCopy];
+    if (accessGroup) query[(__bridge id) kSecAttrAccessGroup] = accessGroup;
 
     OSStatus err = SecItemAdd((__bridge CFDictionaryRef) query, NULL);
-    if (err == errSecDuplicateItem && fc_deleteKeychainStringForKey(key)) err = SecItemAdd((__bridge CFDictionaryRef) query, NULL);
+    if (err == errSecDuplicateItem && fc_deleteGroupKeychainStringForKey(key, accessGroup)) err = SecItemAdd((__bridge CFDictionaryRef) query, NULL);
     if (err != errSecSuccess) NSLog(@"Keychain error: SecItemAdd failed for key %@: %d", key, (int) err);
     return err == errSecSuccess;
 }
 
-static __inline__ __attribute__((always_inline)) BOOL fc_setKeychainStringForKey(NSString *key, NSString *value) { return fc_setKeychainStringForKeyWithAccessibility(key, value, kSecAttrAccessibleAlways); }
+static __inline__ __attribute__((always_inline)) BOOL fc_setGroupKeychainStringForKey(NSString *key, NSString *value, NSString *accessGroup)
+{
+    return fc_setGroupKeychainStringForKeyWithAccessibility(key, value, kSecAttrAccessibleAlways, accessGroup);
+}
+
+static __inline__ __attribute__((always_inline)) NSString *fc_keychainStringForKey(NSString *key, BOOL *outKeychainError)
+{
+    return fc_groupKeychainStringForKey(key, outKeychainError, nil);
+}
+
+static __inline__ __attribute__((always_inline)) BOOL fc_deleteKeychainStringForKey(NSString *key)
+{
+    return fc_deleteGroupKeychainStringForKey(key, nil);
+}
+
+static __inline__ __attribute__((always_inline)) BOOL fc_setKeychainStringForKeyWithAccessibility(NSString *key, NSString *value, CFTypeRef accessibility)
+{
+    return fc_setGroupKeychainStringForKeyWithAccessibility(key, value, accessibility, nil);
+}
+
+static __inline__ __attribute__((always_inline)) BOOL fc_setKeychainStringForKey(NSString *key, NSString *value)
+{
+    return fc_setGroupKeychainStringForKeyWithAccessibility(key, value, kSecAttrAccessibleAlways, nil);
+}
 
 #endif
