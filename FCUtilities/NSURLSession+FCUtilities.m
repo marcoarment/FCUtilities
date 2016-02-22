9b87aea1333ceb5cecf4a9d62ea6cb47797068da
Marco Arment <me@marco.org>
2016-02-21 22:04:05 -0500

    This should be an instance method, not class

--- FCUtilities/NSURLSession+FCUtilities.m
+++ FCUtilities/NSURLSession+FCUtilities.m
@@ -7,13 +7,12 @@
 
 @implementation NSURLSession (FCUtilities)
 
-+ (NSData * _Nullable)fc_sendSynchronousRequest:(NSURLRequest * _Nonnull)request returningResponse:(NSURLResponse * _Nullable * _Nullable)response error:(NSError * _Nullable * _Nullable)error
+- (NSData * _Nullable)fc_sendSynchronousRequest:(NSURLRequest * _Nonnull)request returningResponse:(NSURLResponse * _Nullable * _Nullable)response error:(NSError * _Nullable * _Nullable)error
 {
-    NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration]];
     dispatch_semaphore_t done = dispatch_semaphore_create(0);
     __block NSData *data = nil;
 
-    [[session dataTaskWithRequest:request completionHandler:^(NSData * _Nullable gotData, NSURLResponse * _Nullable gotResponse, NSError * _Nullable gotError) {
+    [[self dataTaskWithRequest:request completionHandler:^(NSData * _Nullable gotData, NSURLResponse * _Nullable gotResponse, NSError * _Nullable gotError) {
         data = gotData;
         if (response) *response = gotResponse;
         if (error) *error = gotError;
@@ -23,5 +22,6 @@
     dispatch_semaphore_wait(done, DISPATCH_TIME_FOREVER);
     return data;
 }
+
 
 @end
